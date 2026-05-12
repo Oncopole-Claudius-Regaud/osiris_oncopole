89bc99b4c366b5759488c6004089411253e4bb3f
Gutkowski Florian <gutkowski@iuct-oncopole.fr>
2026-05-12 09:53:12 +0200

    suppresion trigger , creation orchestrateur main

--- etl_iris.py
+++ etl_iris.py
@@ -78,7 +78,6 @@ def extract_data_from_iris_osiris(**kwargs):
 with DAG(
     dag_id='etl_iris_data',
     default_args=default_args,
-    start_date=datetime(2025, 4, 3),
     catchup=False,
     tags=["lymphome-data", "osiris", "PROD"],
 ) as dag:
